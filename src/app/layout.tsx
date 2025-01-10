c40faa0699110db5772d03e2c5a199084b045014
Meshack Kipchirchir <ChirchirMeshack20@gmail.com>
2025-01-10 09:19:01 +0300

    Finished MVP of portfolio website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,69 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { DATA } from "@/data/resume";
+import {ThemeProvider} from "@/components/theme-provider";
+import {TooltipProvider} from "@/components/ui/tooltip";
+import Navbar from "@/components/navbar";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
+export const metadata: Metadata = {
+  metadataBase: new URL(DATA.url),
+  title: {
+    default: DATA.name,
+    template: `%s | ${DATA.name}`,
+  },
+  description: DATA.description,
+  twitter: {
+    card: "summary_large_image",
+    creator: DATA.contact.social.X.url,
+    site: DATA.url,
+    title: DATA.name,
+    description: DATA.description,
+    images: [DATA.avatarUrl],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  verification: {
+    google: "",
+    yandex: "",
+    bing: "",
+  },
+  openGraph: {
+    type: "website",
+    url: DATA.url,
+    title: DATA.name,
+    description: DATA.description,
+    locale: "en_US",
+    images: [
+      {
+        url: DATA.avatarUrl,
+        alt: DATA.name,
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
@@ -21,7 +73,13 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <TooltipProvider delayDuration={0}>
+          {children}
+          <Navbar></Navbar>
+        </TooltipProvider>
+      </ThemeProvider>
+
       </body>
     </html>
   );
---
+++ src/components/ui/avatar.tsx
@@ -0,0 +1,80 @@
+"use client";
+import * as React from "react";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import { cn } from "@/lib/utils";
+
+const Avatar = React.forwardRef<
+    React.ElementRef<typeof AvatarPrimitive.Root>,
+    React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root> & {
+    size?: number;
+    src?: string;
+    alt?: string;
+    fallbackSrc?: string;
+    fallbackAlt?: string;
+    fallbackIcon?: React.ReactNode;
+    fallbackIconSize?: number;
+}
+>(({ className, size = 24, src, alt, fallbackSrc, fallbackAlt, fallbackIcon, fallbackIconSize = 24, ...props }, ref) => (
+    <AvatarPrimitive.Root
+        ref={ref}
+        className={cn('relative flex items-center justify-center rounded-full', className)}
+        style={{ height: size, width: size }}
+        {...props}
+    >
+        {src ? (
+            <AvatarImage
+                className="object-cover h-full w-full"
+                src={src}
+                alt={alt}
+            />
+        ) : (
+            <AvatarFallback
+                className="flex items-center justify-center h-full w-full rounded-full bg-muted"
+            >
+                {fallbackIcon ? (
+                    React.cloneElement(fallbackIcon, { size: fallbackIconSize })
+                ) : (
+                    fallbackSrc ? (
+                        <AvatarImage
+                            src={fallbackSrc}
+                            alt={fallbackAlt}
+                            className="object-cover h-full w-full"
+                        />
+                    ) : (
+                        <span>{fallbackAlt}</span>
+                    )
+                )}
+            </AvatarFallback>
+        )}
+    </AvatarPrimitive.Root>
+));
+
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = React.forwardRef<
+    React.ElementRef<typeof AvatarPrimitive.Image>,
+    React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+    <AvatarPrimitive.Image
+        ref={ref}
+        className={cn("aspect-square h-full w-full", className)}
+        {...props}
+    />
+));
+
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = React.forwardRef<
+    React.ElementRef<typeof AvatarPrimitive.Fallback>,
+    React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+    <AvatarPrimitive.Fallback
+        ref={ref}
+        className={cn("flex items-center justify-center h-full w-full rounded-full bg-muted", className)}
+        {...props}
+    />
+));
+
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+export { Avatar, AvatarImage, AvatarFallback };
